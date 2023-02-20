4ca3551434d88c2ed0db76b76dc6fdfc903eb76c
jordyorel <jordyorel@protonmail.com>
2023-02-20 21:44:35 +0800

    first commit

---
+++ django_project/books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
